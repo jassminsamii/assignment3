316f3702900292543d49db106baf3b69c2b9349e
jassminsamii <jojoshrief5@gmail.com>
2026-08-28 22:56:17 +0300

    question02

--- assignment/Program.cs
+++ assignment/Program.cs
@@ -4,11 +4,15 @@
     {
         static void Main(string[] args)
         {
-           // question01
+            // question01
 
-           // double[] prices = { 25.5, 40.0, 33.75 };
-           // Console.WriteLine(prices[1]);
-           //==============================================================//
+            // double[] prices = { 25.5, 40.0, 33.75 };
+            // Console.WriteLine(prices[1]);
+            //==============================================================//
+
+            //question02
+            //int[,] shelfCopies = { { 3, 5 }, { 1, 4 } };
+            //Console.WriteLine(shelfCopies[1, 0]);
         }
     }
 }
