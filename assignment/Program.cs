41a84c04a4e34949ec3d5e1eed7eacbdc600a850
jassminsamii <jojoshrief5@gmail.com>
2026-08-28 22:54:08 +0300

    question01

--- assignment/Program.cs
+++ assignment/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            
+           // question01
+
+           // double[] prices = { 25.5, 40.0, 33.75 };
+           // Console.WriteLine(prices[1]);
+           //==============================================================//
         }
     }
 }
